c7e04916d3992d1ecb79427bc1a3db9661fff6fa
JanakaChinthana <janakachinthana1@gmail.com>
2021-04-30 21:07:43 +0530

    Condition changes

--- src/pack1/ReaCSVExample1.java
+++ src/pack1/ReaCSVExample1.java
@@ -16,24 +16,36 @@ public class ReaCSVExample1 {
 	while ((line = br.readLine()) != null)   //returns a Boolean value  
 	{  
 		String[] employee = line.split(splitBy);    // use comma as separator  
-		if(line.contains("BSc") ) {
+		if(line.contains("PhD") ) {
 			if(!employee[12].equals(" publications")) {
 				int x = Integer.parseInt(employee[12]);
 				if(x >= 15) {
 					System.out.println(employee[0] + ", " + employee[1] + ", " + employee[2] + ", " + employee[3] + ", " + employee[4] + ", " + employee[5] + ", " + employee[6] + ", " + employee[7] + ", " + employee[8] + ", " + employee[9] + ", " + employee[10] + ", " + employee[11] + ", " + employee[12]);
-					System.out.println("Full Professor");
+					System.out.println("Full Professor \n");
 				}
 				else if(x >= 8) {
 					System.out.println(employee[0] + ", " + employee[1] + ", " + employee[2] + ", " + employee[3] + ", " + employee[4] + ", " + employee[5] + ", " + employee[6] + ", " + employee[7] + ", " + employee[8] + ", " + employee[9] + ", " + employee[10] + ", " + employee[11] + ", " + employee[12]);
-					System.out.println("Associate Professor");
-				}
-				else {
-					System.out.println(employee[0] + ", " + employee[1] + ", " + employee[2] + ", " + employee[3] + ", " + employee[4] + ", " + employee[5] + ", " + employee[6] + ", " + employee[7] + ", " + employee[8] + ", " + employee[9] + ", " + employee[10] + ", " + employee[11] + ", " + employee[12]);
-					System.out.println("Lecturer");
+					System.out.println("Associate Professor \n");
 				}
 			}
 			
 			
+		}
+		else if(line.contains("BSc") ){
+			int x = Integer.parseInt(employee[12]);
+			if(x >= 8) {
+				System.out.println(employee[0] + ", " + employee[1] + ", " + employee[2] + ", " + employee[3] + ", " + employee[4] + ", " + employee[5] + ", " + employee[6] + ", " + employee[7] + ", " + employee[8] + ", " + employee[9] + ", " + employee[10] + ", " + employee[11] + ", " + employee[12]);
+				System.out.println("Senior Lecturer \n");
+			}
+			else if(x < 8) {
+				System.out.println(employee[0] + ", " + employee[1] + ", " + employee[2] + ", " + employee[3] + ", " + employee[4] + ", " + employee[5] + ", " + employee[6] + ", " + employee[7] + ", " + employee[8] + ", " + employee[9] + ", " + employee[10] + ", " + employee[11] + ", " + employee[12]);
+				System.out.println("Lecturer \n");
+			}
+			
+		}
+		else {
+			System.out.println(employee[0] + ", " + employee[1] + ", " + employee[2] + ", " + employee[3] + ", " + employee[4] + ", " + employee[5] + ", " + employee[6] + ", " + employee[7] + ", " + employee[8] + ", " + employee[9] + ", " + employee[10] + ", " + employee[11] + ", " + employee[12]);
+			System.out.println("Instructor \n");
 		}
 	}  
 	}   
